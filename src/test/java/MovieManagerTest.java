a6126224f3263e2aac8a8ae59a7f3266f893d08f
Natalie-Roach <natalie-roach@yandex.ru>
2022-08-16 21:02:43 +0300

    Modified src/

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -51,7 +51,7 @@ public class MovieManagerTest {
 
     @Test
 
-    public void shouldFindLastMoviesIfMoreThanLimit() {
+    public void shouldFindLastMoviesIfLimit12() {
         MovieManager manager = new MovieManager(12);
         manager.addMovie("movie1");
         manager.addMovie("movie2");
@@ -69,6 +69,76 @@ public class MovieManagerTest {
         String[] actual = manager.findLastMovies();
 
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    //ДОРАБОТКА ЗАДАНИЯ
+    @Test
+
+    public void shouldFindLastMoviesIfQuantityOfMoviesLessThanLimit() {
+        MovieManager manager = new MovieManager(10);
+        manager.addMovie("movie1");
+        manager.addMovie("movie2");
+        manager.addMovie("movie3");
+        manager.addMovie("movie4");
+        manager.addMovie("movie5");
+        manager.addMovie("movie6");
+        manager.addMovie("movie7");
+
+        String[] expected = {"movie7", "movie6", "movie5", "movie4", "movie3", "movie2", "movie1"};
+        String[] actual = manager.findLastMovies();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldFindLastMoviesIfQuantityOfMoviesSameAsLimit() {
+        MovieManager manager = new MovieManager(10);
+        manager.addMovie("movie1");
+        manager.addMovie("movie2");
+        manager.addMovie("movie3");
+        manager.addMovie("movie4");
+        manager.addMovie("movie5");
+        manager.addMovie("movie6");
+        manager.addMovie("movie7");
+        manager.addMovie("movie8");
+        manager.addMovie("movie9");
+        manager.addMovie("movie10");
+
+        String[] expected = {"movie10", "movie9", "movie8", "movie7", "movie6", "movie5", "movie4", "movie3", "movie2", "movie1"};
+        String[] actual = manager.findLastMovies();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void shouldFindLastMoviesIfQuantityOfMoviesMoreThanLimit() {
+        MovieManager manager = new MovieManager(10);
+        manager.addMovie("movie1");
+        manager.addMovie("movie2");
+        manager.addMovie("movie3");
+        manager.addMovie("movie4");
+        manager.addMovie("movie5");
+        manager.addMovie("movie6");
+        manager.addMovie("movie7");
+        manager.addMovie("movie8");
+        manager.addMovie("movie9");
+        manager.addMovie("movie10");
+        manager.addMovie("movie11");
+        manager.addMovie("movie12");
+        manager.addMovie("movie13");
+        manager.addMovie("movie14");
+        manager.addMovie("movie15");
+
+        String[] expected = {"movie15", "movie14", "movie13", "movie12", "movie11", "movie10", "movie9", "movie8", "movie7", "movie6"};
+        String[] actual = manager.findLastMovies();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
     }
 
 }
